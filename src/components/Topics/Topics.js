dfaabe3fad8b6d069335400ca12946231dd5b356
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-10-11 23:04:41 +0600

    Topic page banner section added

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Topics = () => {
   return (
     <div>
+      <Banner></Banner>
       <h2>This is topic page</h2>
       <h3>This page is about Topics</h3>
     </div>
