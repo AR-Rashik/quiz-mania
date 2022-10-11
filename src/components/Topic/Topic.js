a4fbc481e583c721e45d895d87a6ef9ff72f1e13
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-10-12 00:24:53 +0600

    Topics section added

---
+++ src/components/Topic/Topic.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Topic = ({topic}) => {
+
+  const {id, name, logo, total} = topic;
+
+  return (
+    <div>
+      <div className="rounded flex items-center justify-center w-full">
+        <div className=" rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+          <img src={logo} alt="" className="object-cover object-center w-full rounded-t-md dark:bg-gray-700" />
+          <div className="flex flex-col justify-between p-6 space-y-8">
+              <div className="space-y-2">
+                <div className='flex items-center justify-between mb-5'>
+                  <div>{name}</div>
+                  <div>Total Quiz: {total}</div>
+                </div>
+                <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-400 dark:text-gray-900">Start practice</button>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topic;
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import Banner from '../Banner/Banner';
+import Topic from '../Topic/Topic';
 
 const Topics = () => {
+
+  const topicsObject = useLoaderData();
+  // console.log(topicsObject);
+  const topics = topicsObject.data;
+  console.log(topics);
+
   return (
     <div>
       <Banner></Banner>
-      <h2>This is topic page</h2>
-      <h3>This page is about Topics</h3>
+      <div className="container w-10/12 mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 pt-6 gap-6 mb-10">
+        {
+          topics.map( topic => <Topic
+            key={topic.id}
+            topic={topic}
+          ></Topic>)
+        }
+      </div>
     </div>
   );
 };
