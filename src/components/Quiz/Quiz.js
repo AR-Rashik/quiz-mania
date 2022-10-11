be2f6a7b1b67c0155e259df318ec5a4a39f43d89
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-10-12 04:21:02 +0600

    sww correct answer by eye icon added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuizOptions from '../QuizOptions/QuizOptions';
+import { EyeIcon } from '@heroicons/react/24/solid';
 
 const Quiz = ({quiz, questions}) => {
 
@@ -8,10 +9,20 @@ const Quiz = ({quiz, questions}) => {
   const quizIndex = questions.indexOf(quiz);
   let quizNumber = quizIndex + 1;
 
+  const [answer, setAnswer] = useState();
+
+  const handleBtn = () => {
+    const message = <p>Correct answer is: {correctAnswer}</p>
+    setAnswer(message);
+  }
+
+  
+
   return (
     <div className='mx-auto mb-5'>
-      <div className='bg-gray-300'>
+      <div className='bg-gray-300 relative'>
         <h4>Quiz {quizNumber} : {question}</h4>
+        <EyeIcon onClick={handleBtn}  className="h-6 w-6 text-gray-700 absolute top-1 right-2"/>
         <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 p-3 gap-3'>
           {
             options.map(option => <QuizOptions
@@ -22,6 +33,9 @@ const Quiz = ({quiz, questions}) => {
             ></QuizOptions>)
           }
         </div>
+        {
+          answer
+        }
       </div>
       
     </div>
