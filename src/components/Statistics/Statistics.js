58c0b10744937586e411e07b076016f82b63c1a4
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-10-12 14:52:24 +0600

    Statistics bar of topics and totals are added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+
+  const [total, setTotal] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://openapi.programming-hero.com/api/quiz')
+        .then(data => {
+          const topics = data.data.data;
+          console.log(topics);
+          const topicsData = topics.map(topic => {
+            const topicName = topic.name;
+            const totalQuiz = topic.total;
+
+            const singleTopic = {
+              name: topicName,
+              total: totalQuiz
+            }
+
+            return singleTopic;
+          })
+
+          console.log(topicsData);
+          setTotal(topicsData);
+        });
+  }, [])
+
+  console.log(total);
+
   return (
-    <div>
-      <h2>This is Statistics page.</h2>
-      <h3>This page is about statistics</h3>
+    <div className='flex justify-center items-center lg:w-1/2 mx-auto md:w-10/12'>
+      <div>
+        <BarChart width={400} height={450} data={total}>
+          <Bar dataKey="total" fill="#8884d8"></Bar>
+          <XAxis dataKey="name" />
+            <YAxis dataKey="total" />
+            <Tooltip></Tooltip>
+        </BarChart>
+      </div>
     </div>
   );
 };
